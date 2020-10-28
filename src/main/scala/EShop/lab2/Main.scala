fc38bde93768fc67f1c66808557fb95582fcb057
Anna Banaszak <anna.banaszak@u2i.com>
2020-10-28 10:00:41 +0100

    fix main

--- src/main/scala/EShop/lab2/Main.scala
+++ src/main/scala/EShop/lab2/Main.scala
@@ -2,17 +2,18 @@ package EShop.lab2
 
 import EShop.lab2.CartActor.AddItem
 import EShop.lab2.CartActor.RemoveItem
-import akka.actor
-import akka.actor.typed
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    val system = actor.ActorSystem("system")
-    val cart = system.actorOf(actor.Props[CartActor], "cart")
+  def classic() : Unit = {
+    import akka.actor.{ActorSystem, Props}
+    val system = ActorSystem("system")
+    val cart = system.actorOf(Props[CartActor], "cart")
     cart ! AddItem("item")
     cart ! RemoveItem("item")
     system.terminate()
+  }
 
-    val typedSystem = typed.ActorSystem[TypedCartActor.Command] =
+  def main(args: Array[String]): Unit = {
+    classic()
   }
 }
